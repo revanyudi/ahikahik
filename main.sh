6fcee3b54201ffc36876b2af485c6aaf2b6c80dd
Lexxy official <93019459+Lexxy24@users.noreply.github.com>
2022-05-19 10:40:50 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,9 @@
-pkg update && pkg upgrade
-pkg install git -y
-pkg install nodejs -y
-pkg install ffmpeg -y
-pkg install imagemagick -y
-pkg install yarn
-git clone https://github.com/Lexxy24/KaguraMD
-cd KaguraMD
-yarn
-node haruka.js
+#!bin/bash
+GREEN='\033[0;32m'
+while : 
+do
+echo "${GREEN} START BOT ANTI LAG..."
+    node kagura.js
+    sleep 1
+
+done
