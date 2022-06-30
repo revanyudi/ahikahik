b04702fafe47704228a4c022a2a16aa2cc226f85
ahikahik <96776244+revanyudi@users.noreply.github.com>
2022-06-30 14:00:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,12 +10,12 @@ global.APIKeys = {
 }
 
 //---------( SETTING )---------//
-global.owner = ['6282279915237','6285789004732','6285724706876']
-global.premium = ['6282279915237']
-global.ownernomer = '6282279915237'
-global.ownername = 'Lexxy Official'
-global.botname = 'KaguraMD'
-global.footer = 'Lexxy Official'
+global.owner = ['6285714417861','6285789004732','6285724706876']
+global.premium = ['6285714417861']
+global.ownernomer = '6285714417861'
+global.ownername = 'BOCILSD'
+global.botname = 'Ember Bot'
+global.footer = 'BOCILSD'
 global.thumb = fs.readFileSync('./image/fake.jpg')
 global.veloriy = fs.readFileSync('./image/kagura.mp4')
 global.sessionName = 'session'
